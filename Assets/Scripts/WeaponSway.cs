548705f79a87bf86976ce34e5212a5a14981336c
Vedal987 <vedal876@gmail.com>
2017-10-14 23:20:48 +0100

    BasicMovement

---
+++ Assets/Scripts/WeaponSway.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSway : MonoBehaviour {
+
+	float amount = 0.02f;
+	float maxAmount = 0.03f;
+	float smooth = 3;
+	private Vector3 def;
+
+	void  Start (){
+		def = transform.localPosition;
+	}
+
+	void  Update (){
+
+		float factorX = -Input.GetAxis("Mouse X") * amount;
+		float factorY = -Input.GetAxis("Mouse Y") * amount;
+
+		factorX = Mathf.Clamp(factorX, -maxAmount, maxAmount);
+		factorY = Mathf.Clamp(factorY, -maxAmount, maxAmount);
+
+		Vector3 Final = new Vector3(def.x+factorX, def.y+factorY, def.z);
+		transform.localPosition = Vector3.Lerp(transform.localPosition, Final, Time.deltaTime * smooth);       
+	}
+}
